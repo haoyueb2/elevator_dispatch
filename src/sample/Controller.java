17dfc57f44689f498a578e402acbc03f49e0e7f4
mooooon333 <742932097@qq.com>
2019-05-06 21:44:33 +0800

    inside all and outside upOrder succeed

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -10,41 +10,102 @@ public class Controller {
     public ChoiceBox elevatorChoice;
     public AnchorPane root;
     public Button[] insideFloorButtons = new Button[21];
+    public Button[] outsideUpButtons = new Button[6];
+    public Button[] outsideDownButtons = new Button[6];
     public Elevator[] elevators = new Elevator[6];
     public Slider[] elevatorSliders = new Slider[6];
-    private void initInsideFloorButton() {
+    private void initInsideFloorButtons() {
         //目的是从1到20，把0空出来
-
-        for(int i = 1; i <= 10; i++) {
+        for(int i = 1; i <= 20; i++) {
             insideFloorButtons[i] = (Button)root.lookup("#floor"+i);
             if(insideFloorButtons[i] != null) {
                 final int chosenFloor = i;
                 insideFloorButtons[i].setOnAction((event) -> {
-
-
-                    String elevatorChoiceValue = (String)elevatorChoice.getValue();
-                    int elevatorIndex = elevatorChoiceValue.charAt(8)-'0';
-                    //请求不是本层才点亮按钮
-                    if(elevators[elevatorIndex].currentFloor != chosenFloor) {
-                        insideFloorButtons[chosenFloor].setStyle("-fx-background-color:#5264AE;");
-                    }
-                    if((elevators[elevatorIndex].status == Elevator.PAUSE || elevators[elevatorIndex].status == Elevator.UP) &&
-                        chosenFloor > elevators[elevatorIndex].currentFloor) {
-
-                        elevators[elevatorIndex].upQueue.add(chosenFloor);
-                    }
+                    handleInsideOrder(chosenFloor);
                 });
             }
+        }
+    }
+    private void initOutsideOrderButtons() {
+        for(int i =1;i <= 5; i++) {
+            outsideUpButtons[i] = (Button)root.lookup("#up"+i);
+            if(outsideUpButtons[i] != null) {
+                outsideUpButtons[i].setOnAction((event) -> {
+                    handleOutsideUpOrder();
+                });
+            }
+        }
 
+    }
+    private void handleOutsideUpOrder() {
+        int outsideCurFloor;
+        String floorValue = (String)floorChoice.getValue();
+        outsideCurFloor =Integer.parseInt(floorValue.substring(5));
+        for(int i = 1; i <= 5; i ++ ) {
+            outsideUpButtons[i].setStyle("-fx-background-color:#5264AE;");
+        }
+        int nearestElevatorIndex = 0;
+        int minDistance = 100;//先设一个很大的数
+        //先在up里边找，是如果Up和Pause的电梯离楼层距离相等时，优先调度Up状态的
+        for(int i = 1; i <= 5; i++) {
+            //由于只要有待处理的上升楼层电梯就是上升状态，所以楼层相等时Up的楼层也得开
+            if(elevators[i].status == Elevator.UP && elevators[i].currentFloor<= outsideCurFloor) {
+                int distance = outsideCurFloor - elevators[i].currentFloor;
+                if(distance < minDistance) {
+                    minDistance = distance;
+                    nearestElevatorIndex = i;
+                }
+            }
+        }
+        for(int i = 1;i <= 5; i++) {
+            if(elevators[i].status == Elevator.PAUSE) {
+                int distance = Math.abs(outsideCurFloor-elevators[i].currentFloor);
+                if(distance < minDistance) {
+                    minDistance = distance;
+                    nearestElevatorIndex = i;
+                }
+            }
+        }
+        if(nearestElevatorIndex != 0) {
+            //此时电梯来自上方下方都有可能
+            if(outsideCurFloor - elevators[nearestElevatorIndex].currentFloor > 0) {
+                elevators[nearestElevatorIndex].upQueue.add(outsideCurFloor);
+            } else if(outsideCurFloor - elevators[nearestElevatorIndex].currentFloor < 0){
+                elevators[nearestElevatorIndex].downQueue.add(outsideCurFloor);
+            } else if(outsideCurFloor - elevators[nearestElevatorIndex].currentFloor == 0) {
+                //电梯开门
+            }
+        }
+
+    }
+    private void handleInsideOrder(int chosenFloor) {
+        String elevatorChoiceValue = (String)elevatorChoice.getValue();
+        int elevatorIndex = elevatorChoiceValue.charAt(8)-'0';
+        //请求不是本层才点亮按钮
+        if(elevators[elevatorIndex].currentFloor != chosenFloor) {
+            insideFloorButtons[chosenFloor].setStyle("-fx-background-color:#5264AE;");
+        }
+        if(chosenFloor > elevators[elevatorIndex].currentFloor) {
+
+            elevators[elevatorIndex].upQueue.add(chosenFloor);
+        }
+        if(chosenFloor < elevators[elevatorIndex].currentFloor) {
+            elevators[elevatorIndex].downQueue.add(chosenFloor);
         }
     }
     public void initialize() {
         elevatorChoice.setValue("elevator1");
-        initInsideFloorButton();
+        floorChoice.setValue("floor1");
+        initInsideFloorButtons();
+        initOutsideOrderButtons();
         for(int i = 1; i <= 5; i++) {
             elevatorSliders[i] = (Slider)root.lookup("#slider"+i);
             elevatorSliders[i].setMin(1);
             elevatorSliders[i].setMax(20);
+            elevatorSliders[i].setShowTickLabels(true);
+            elevatorSliders[i].setShowTickMarks(true);
+            elevatorSliders[i].setMinorTickCount(0);
+            elevatorSliders[i].setMajorTickUnit(1);
 
         }
         for(int i = 1; i <= 5;i++){
--- src/sample/Elevator.java
+++ src/sample/Elevator.java
@@ -3,6 +3,7 @@ package sample;
 
 import javafx.application.Platform;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 public class Elevator implements Runnable {
@@ -14,31 +15,50 @@ public class Elevator implements Runnable {
     public final static int DOWN = 2;
     private int selfElevatorIndex;
     public int status = PAUSE;
-    public PriorityQueue<Integer> upQueue = new PriorityQueue<>();
-    public PriorityQueue<Integer> downQueue = new PriorityQueue<>();
+    public PriorityQueue<Integer> upQueue ;
+
+    public PriorityQueue<Integer> downQueue ;
     private Controller controller;
+    //消除重复，有重复的话很可能导致电梯持续运行到顶或底
+    private class MyPriQueue<E> extends PriorityQueue<E> {
+        MyPriQueue() {
+            super();
+        }
+        //子类构造函数不继承父类的，只能在构造函数里调用
+        MyPriQueue(Comparator<E> cmp) {
+            super(cmp);
+        }
+
+        @Override
+        public boolean add(E e) {
+            boolean isAdded = false;
+            if (!super.contains(e)) {
+                isAdded = super.add(e);
+            }
+            return isAdded;
+        }
+    }
     public Elevator(int selfElevatorIndex, Controller controller){
         this.selfElevatorIndex = selfElevatorIndex;
         this.controller = controller;
+        Comparator<Integer> cmp;
+        cmp = (o1, o2) -> o2 -o1;
+        upQueue = new MyPriQueue<>();
+        downQueue = new MyPriQueue<>(cmp);
     }
     @Override
     public void run() {
 
         while(true){
-            System.out.println("run!"+ selfElevatorIndex);
+            System.out.println("detectUp!elevator:"+ selfElevatorIndex);
             while(upQueue.size() > 0) {
                 status = UP;
                 currentFloor++;
                 if(upQueue.peek() == currentFloor) {
                     upQueue.poll();
-                    //System.out.println("arrive!"+ currentFloor);
                     controller.insideFloorButtons[currentFloor].setStyle(null);
-                    //Platform.runLater(()->controller.insideFloorButtons[currentFloor].setStyle(null));
                 }
-
-                System.out.println("run!"+ selfElevatorIndex+"floor:"+selfElevatorIndex);
                 controller.elevatorSliders[selfElevatorIndex].setValue(currentFloor);
-                //Platform.runLater(()-> controller.elevatorSliders[selfElevatorIndex].setValue(currentFloor));
                 try {
                     Thread.sleep(1000);
                 }
@@ -47,11 +67,14 @@ public class Elevator implements Runnable {
                 }
             }
             while(downQueue.size() > 0) {
+                System.out.println("detectDown!elevator:"+ selfElevatorIndex);
                 status = DOWN;
+                currentFloor--;
                 if(downQueue.peek() == currentFloor) {
                     downQueue.poll();
+                    controller.insideFloorButtons[currentFloor].setStyle(null);
                 }
-                currentFloor--;
+                controller.elevatorSliders[selfElevatorIndex].setValue(currentFloor);
                 try {
                     Thread.sleep(1000);
                 }
